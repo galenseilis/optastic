aac0f469922d085be45b44aab925a61b03ce692b
galenseilis <galen.seilis@seilis.ca>
2023-07-02 21:43:47 -0700

    Rough draft of multivariate gaussian error.

--- src/optastic/models.py
+++ src/optastic/models.py
@@ -6,26 +6,49 @@ import suggestions
 class NormToNorm:
     """IO multivariate Gaussian uncertainty."""
 
-    def __init__(self, model, X, Y, loss=None):
+    def __init__(self, model, X, Y, loss):
         """
         model: Trained SKlearn regressor.
         X numpy.ndarray: Input data.
         Y numpy.ndarry: Output data.
         """
 
-        if loss is None:
-
-            def loss(x, y):
-                """Default MSE."""
-                return np.mean(np.power(x - y, 2))
-
-        X_dim = X.shape[1]
-        Y_dim = Y.shape[1]
+        X_mean = np.mean(X, axis=0)
+        X_residual = X - X_mean
+        X_min_residual = np.min(X_residual, axis=0)
+        X_max_residual = np.max(X_residual, axis=0)
+        X_max_square_residuals = np.max(np.square(X_residual), axis=0)
+        
+        
+        Y_mean = np.mean(Y, axis=0)
+        Y_residual = Y - Y_mean
+        Y_min_residual = np.min(Y_residual, axis=0)
+        Y_max_residual = np.max(Y_residual, axis=0)
+        Y_max_square_residuals = np.max(np.square(Y_residual), axis=0)
 
         def objective(trial):
-            X_corr = suggestions.suggest_corr(trial, "R_x", X_dim)
-            Y_corr = suggestions.suggest_corr(trial, "R_y", Y_dim)
+            # Sample input error distribution
+            X_corr = suggestions.suggest_corr(trial, "x_corr", X.shape[1])
+            X_mu = [trial.suggest_float(f'x_mu_{k}', i,j) for k,(i,j) in enumerate(zip(X_min_residual, X_max_residual))]
+            X_sigma = [trial.suggest_float(f'x_sigma_{k}', 0.0, s) for k,s in enumerate(X_max_square_residuals)]
+            X_sigma = np.array(X_sigma)
+            X_cov = np.outer((X_sigma,)*2) * X_corr
+            X_sample = np.random.multivariate_normal(X_mu, X_cov, size=X.shape[0])
 
+            # Sample output error distribution
+            Y_corr = suggestions.suggest_corr(trial, "y_corr", Y.shape[1])
+            Y_mu = [trial.suggest_float(f'y_mu_{k}', i,j) for k,(i,j) in enumerate(zip(Y_min_residual, Y_max_residual))]
+            Y_sigma = [trial.suggest_float(f'y_sigma_{k}', 0.0, s) for k,s in enumerate(y_max_square_residuals)]
+            Y_sigma = np.array(Y_sigma)
+            Y_cov = np.outer((Y_sigma,)*2) * Y_corr
+            Y_sample = np.random.multivariate_normal(Y_mu, Y_cov, size=Y.shape[0])
+
+            # Make noisy prediction
+            Y_hat = model.predict(X + X_sample) + Y_sample
+
+
+            return loss(Y, Y_hat)
+            
         self.objective = objective
 
     def fit(self, n_trials=3, *args, **kwargs):
@@ -34,3 +57,5 @@ class NormToNorm:
         study.optimize(self.objective, n_trials=n_trials)
         return study
 
+# TODO: Real-to-classification for misclassication problems.
+# TODO: Real-to-non-negative integer regression for count data

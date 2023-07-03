78025fda95825e4e69c333e8cf8c8306849db84d
galenseilis <galen.seilis@seilis.ca>
2023-07-02 21:05:36 -0700

    moved corr suggest to suggestions

---
+++ src/optastic/suggestions.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def suggest_corr(self, trial, symbol, dim):
+        """Suggest a correlation matrix."""
+        R = np.zeros(shape=(dim, dim), dtype=object)
+        for i in range(dim):
+            for j in range(dim):
+                R[i, j] = trial.suggest_float(f"{symbol}_{i}_{j}", -1.0, 1.0)
+        R = (R.T @ R) / dim
+        np.fill_diagonal(R, 1.0)
+        return R
